73b795ad44f40d6b1127af2bcfc75da80c869c48
GoldenKevin <GoldenKevin@d612c082-bd41-1c40-8be0-570093e9c2c9>
2012-08-22 13:59:29 +0000

    * Added additional data for ship maps.

---
+++ trunk/src/kvjcompiler/map/structure/Ship.java
@@ -0,0 +1,53 @@
+/*
+ *  KvJ Compiler for XML WZ data files
+ *  Copyright (C) 2010-2012  GoldenKevin
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import kvjcompiler.IStructure;
+import kvjcompiler.LittleEndianWriter;
+import kvjcompiler.Size;
+
+/**
+ *
+ * @author GoldenKevin
+ */
+public class Ship implements IStructure {
+	private String shipObj;
+	private byte shipKind;
+
+	@Override
+	public void setProperty(String key, String value) {
+		if (key.equals("shipObj")) {
+			this.shipObj = value;
+		} else if (key.equals("shipKind")) {
+			this.shipKind = Byte.parseByte(value);
+		}
+	}
+
+	@Override
+	public int size() {
+		int size = shipObj.length() + 1; //shipObj
+		size += Size.BYTE; //shipKind
+
+		return size;
+	}
+
+	@Override
+	public void writeBytes(LittleEndianWriter lew) {
+		lew.writeNullTerminatedString(shipObj);
+		lew.writeByte(shipKind);
+	}
+}
